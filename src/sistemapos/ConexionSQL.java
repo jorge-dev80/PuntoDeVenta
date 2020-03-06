17ff1fca30e9baf777e686c730aa4b361cfb22d4
AntonioDavid-LT <david-5200@outlook.com>
2020-03-06 17:01:15 -0600

    Se agrego el codigo para la conexion a la base de datos

--- src/sistemapos/ConexionSQL.java
+++ src/sistemapos/ConexionSQL.java
@@ -1,14 +1,25 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package sistemapos;
 
-/**
- *
- * @author david
- */
+//LIBRERIAS
+import java.sql.*;
+import javax.swing.*;
+
 public class ConexionSQL {
-    
+
+    //VARIABLES
+    static Connection cn;
+    static Statement st;
+    static ResultSet rs;
+
+    //METODO PARA HACER LA CONEXION A LA BASE DE DATOS
+    public void conectar() {
+        try {
+            String url = "jdbc:mariadb://localhost:0000/nombreDB";
+            cn = DriverManager.getConnection(url, "Usuario", "Password");
+            st = cn.createStatement();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "No se ha podido conectar\n" + e);
+        }
+    }
+
 }
