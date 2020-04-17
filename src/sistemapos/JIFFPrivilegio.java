04fadffbcad289373ea483d17adfabc5e1e267d7
AntonioDavid-LT <david-5200@outlook.com>
2020-04-16 23:53:56 -0500

    Se completo el diseño del módulo de privilegios

--- src/sistemapos/JIFFPrivilegio.java
+++ src/sistemapos/JIFFPrivilegio.java
@@ -26,22 +26,208 @@ public class JIFFPrivilegio extends javax.swing.JInternalFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 394, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 274, Short.MAX_VALUE)
-        );
+        jBGuardarNuevo = new javax.swing.JButton();
+        jBGuardarCerrar = new javax.swing.JButton();
+        jBAgregar = new javax.swing.JButton();
+        jBEliminar = new javax.swing.JButton();
+        jBRetroceder = new javax.swing.JButton();
+        jBAvanzar = new javax.swing.JButton();
+        jBEditar = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField1 = new javax.swing.JTextField();
+        jTextField2 = new javax.swing.JTextField();
+
+        setClosable(true);
+        setIconifiable(true);
+        setMaximizable(true);
+        setResizable(true);
+        setTitle("Módulo De Privilegios");
+        setPreferredSize(new java.awt.Dimension(550, 300));
+        getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jBGuardarNuevo.setText("Guardar y Nuevo");
+        jBGuardarNuevo.setEnabled(false);
+        jBGuardarNuevo.setPreferredSize(new java.awt.Dimension(120, 32));
+        jBGuardarNuevo.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBGuardarNuevoActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBGuardarNuevo, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 20, -1, -1));
+
+        jBGuardarCerrar.setText("Guardar y Cerrar");
+        jBGuardarCerrar.setEnabled(false);
+        jBGuardarCerrar.setPreferredSize(new java.awt.Dimension(120, 32));
+        jBGuardarCerrar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBGuardarCerrarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBGuardarCerrar, new org.netbeans.lib.awtextra.AbsoluteConstraints(150, 20, -1, -1));
+
+        jBAgregar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/agregar.png"))); // NOI18N
+        jBAgregar.setPreferredSize(new java.awt.Dimension(32, 32));
+        jBAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBAgregarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(280, 20, -1, -1));
+
+        jBEliminar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/eliminar.png"))); // NOI18N
+        jBEliminar.setPreferredSize(new java.awt.Dimension(32, 32));
+        jBEliminar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBEliminarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBEliminar, new org.netbeans.lib.awtextra.AbsoluteConstraints(322, 20, -1, -1));
+
+        jBRetroceder.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/retroceder.png"))); // NOI18N
+        jBRetroceder.setPreferredSize(new java.awt.Dimension(32, 32));
+        jBRetroceder.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBRetrocederActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBRetroceder, new org.netbeans.lib.awtextra.AbsoluteConstraints(364, 20, -1, -1));
+
+        jBAvanzar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/avanzar.png"))); // NOI18N
+        jBAvanzar.setPreferredSize(new java.awt.Dimension(32, 32));
+        jBAvanzar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBAvanzarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBAvanzar, new org.netbeans.lib.awtextra.AbsoluteConstraints(406, 20, -1, -1));
+
+        jBEditar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/img/editar.png"))); // NOI18N
+        jBEditar.setPreferredSize(new java.awt.Dimension(32, 32));
+        jBEditar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBEditarActionPerformed(evt);
+            }
+        });
+        getContentPane().add(jBEditar, new org.netbeans.lib.awtextra.AbsoluteConstraints(448, 20, -1, -1));
+
+        jLabel1.setText("ID Privilegio:");
+        getContentPane().add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 100, -1, -1));
+
+        jLabel2.setText("Privilegio:");
+        getContentPane().add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 150, -1, -1));
+
+        jTextField1.setEditable(false);
+        jTextField1.setPreferredSize(new java.awt.Dimension(200, 25));
+        getContentPane().add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 100, -1, -1));
+
+        jTextField2.setEditable(false);
+        jTextField2.setPreferredSize(new java.awt.Dimension(200, 25));
+        getContentPane().add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(100, 150, -1, -1));
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void jBGuardarNuevoActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardarNuevoActionPerformed
+        // GUARDAR Y NUEVO:
+
+        insertar();
+        jTFNombre.setText("");
+        jPFPassword.setText("");
+        jPFConfirmarPW.setText("");
+        jCBRol.setSelectedIndex(0);
+        jCBEmpresa.setSelectedIndex(0);
+
+    }//GEN-LAST:event_jBGuardarNuevoActionPerformed
+
+    private void jBGuardarCerrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBGuardarCerrarActionPerformed
+        // GUARDAR Y CERRAR:
+
+        if(editar){
+            editar();
+        }else{
+            insertar();
+        }
+        this.show(false);
+
+    }//GEN-LAST:event_jBGuardarCerrarActionPerformed
+
+    private void jBAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAgregarActionPerformed
+        // AGREGAR:
+
+        jTFNombre.setEnabled(true);
+        jPFPassword.setEnabled(true);
+        jPFConfirmarPW.setEnabled(true);
+        jCBRol.setEnabled(true);
+        jCBEmpresa.setEnabled(true);
+        jBGuardarNuevo.setEnabled(true);
+        jBGuardarCerrar.setEnabled(true);
+        jBAgregar.setEnabled(false);
+        jBEliminar.setEnabled(false);
+        jBRetroceder.setEnabled(false);
+        jBAvanzar.setEnabled(false);
+        jBEditar.setEnabled(false);
+        jTFNombre.setText("");
+        jPFPassword.setText("");
+        jPFConfirmarPW.setText("");
+        jCBRol.setSelectedIndex(0);
+        jCBEmpresa.setSelectedIndex(0);
+
+    }//GEN-LAST:event_jBAgregarActionPerformed
+
+    private void jBEliminarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBEliminarActionPerformed
+        // ELIMINAR:
+
+        eliminar();
+
+    }//GEN-LAST:event_jBEliminarActionPerformed
+
+    private void jBRetrocederActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBRetrocederActionPerformed
+        // RETROCEDER:
+
+        retroceder();
+
+    }//GEN-LAST:event_jBRetrocederActionPerformed
+
+    private void jBAvanzarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBAvanzarActionPerformed
+        // AVANZAR:
+
+        avanzar();
+
+    }//GEN-LAST:event_jBAvanzarActionPerformed
+
+    private void jBEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBEditarActionPerformed
+        // EDITAR:
+
+        jTFNombre.setEnabled(true);
+        jPFPassword.setEnabled(true);
+        jPFConfirmarPW.setEnabled(true);
+        jCBRol.setEnabled(true);
+        jCBEmpresa.setEnabled(true);
+        jBGuardarNuevo.setEnabled(false);
+        jBGuardarCerrar.setEnabled(true);
+        jBAgregar.setEnabled(false);
+        jBEliminar.setEnabled(false);
+        jBRetroceder.setEnabled(false);
+        jBAvanzar.setEnabled(false);
+        jBEditar.setEnabled(false);
+        editar = true;
+
+    }//GEN-LAST:event_jBEditarActionPerformed
+
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jBAgregar;
+    private javax.swing.JButton jBAvanzar;
+    private javax.swing.JButton jBEditar;
+    private javax.swing.JButton jBEliminar;
+    private javax.swing.JButton jBGuardarCerrar;
+    private javax.swing.JButton jBGuardarNuevo;
+    private javax.swing.JButton jBRetroceder;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
     // End of variables declaration//GEN-END:variables
 
 }
